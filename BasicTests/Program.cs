b581a8af90a6eadaf4ec4f39ed603e65da40b081
CalamityLorenzo <CalamityLorenzo@users.noreply.github.com>
2026-09-21 21:44:15 +0100

    wefwef

--- BasicTests/Program.cs
+++ BasicTests/Program.cs
@@ -1,2 +1,25 @@
-﻿using var game = new BasicTests.Game3();
-game.Run();
+﻿
+if(args.Length > 0)
+{
+    int.TryParse(args[0], out int gameNumber);
+    if(gameNumber == 3)
+    {
+        using var game = new BasicTests.Game3();
+        game.Run();
+    }
+    if (gameNumber == 2)
+    {
+        using var game = new BasicTests.Game2();
+        game.Run();
+    }
+    if (gameNumber == 1)
+    {
+        using var game = new BasicTests.Game1();
+        game.Run();
+    }
+}
+else
+{
+    using var game = new BasicTests.Game2();
+    game.Run();
+}
